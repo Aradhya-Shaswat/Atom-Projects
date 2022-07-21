d16a7a9ccdbc5bba63a69a8e2234f69b9ed7211d
Arsh_DEV <100127769+Aradhya-Shaswat@users.noreply.github.com>
2022-07-21 19:54:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function LoadUp() {
+  window.alert("Disclaimer! This Website is under development; any bug shall be reported to n-1723-13@dpspatna.com")
+  document.write("<p align='center'><b>FriendBase Prototype</b></p>");
+
+}
+
+
+LoadUp();
